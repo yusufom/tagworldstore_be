f695e5fe85167e7023c3de6fd1e7d831a2220870
yusufom <yusufolamilekanmuktar@gmail.com>
2024-06-26 10:22:34 +0100

    account activation

--- tagworldstore/settings.py
+++ tagworldstore/settings.py
@@ -247,6 +247,6 @@ CLOUDINARY_STORAGE = {
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'ACTIVATION_URL': '/activate?uid={uid}&token={token}',
     'SEND_ACTIVATION_EMAIL': True,
 }
